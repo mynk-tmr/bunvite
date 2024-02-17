986866dbe4724e648067eb170124d60ad9ae066d
Mayank <146926037+mynk-tmr@users.noreply.github.com>
2024-02-17 23:31:27 +0530

    change routes to using <Route/> components

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,19 +1,27 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import HomePage from "../root/page";
 
 export default function Router() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      Component: HomePage,
-    },
-    {
-      path: "*",
-      element: (
-        <h1 className="text-red-500 text-3xl text-center">Page not found</h1>
-      ),
-    },
-  ]);
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" Component={HomePage} />
+        <Route
+          path="*"
+          element={
+            <h1 className="text-red-500 text-3xl text-center">
+              Page not found
+            </h1>
+          }
+        />
+      </>
+    )
+  );
 
   return (
     <>
